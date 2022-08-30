5ff4fc35031eec9257ba8b4020c5615c7087eb35
Julio Bazalar <jbzuloaga@gmail.com>
2022-08-30 12:13:38 -0500

    Add smooth scroll and navbar links

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -7,7 +7,7 @@ import {HiOutlineChevronDoubleUp, HiOutLineChevronDoubleUp} from 'react-icons/hi
 
 const Contact = () => {
   return (
-    <div className='w-full lg:h-screen'>
+    <div id='contact' className='w-full lg:h-screen'>
       <div className='max-w-[1240px] m-auto px-2 py-16 w-full'>
         <p className='text-xl tracking-widest uppercase text-[#5651e5]'> Contact </p>
         <h2 className='py-4'> Get in Touch</h2>
@@ -76,7 +76,7 @@ const Contact = () => {
         <div className='flex justify-center py-12'>
           <Link href='/'>
             <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-              <HiOutlineChevronDoubleUp className='text-[#5651e5] ' size={30}/>
+              <HiOutlineChevronDoubleUp className='text-[#5651e5]' size={30}/>
             </div>
           </Link>
         </div>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,18 +1,30 @@
 import Link from 'next/link';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {AiOutlineClose, AiOutlineMail, AiOutlineMenu} from 'react-icons/ai';
 import {FaGithub, FaLinkedinIn} from 'react-icons/fa';
 import {BsFillPersonLinesFill} from 'react-icons/bs';
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
+  const [shadow, setShadow] = useState(false);
 
   const handleNav = () => {
     setNav(!nav);
   }
 
+  useEffect(() => {
+    const handleShadow = () => {
+      if (window.scrollY >= 90) {
+        setShadow(true);
+      }else{
+        setShadow(false);
+      }
+    };
+    window.addEventListener('scroll', handleShadow);
+  }, []);
+
   return (
-    <div className='fixed w-full h-20 shadow-xl z-[100]'>
+    <div className={shadow ? 'fixed w-full h-20 shadow-xl z-[100]' : 'fixed w-full h-20 z-[100]'}>
       <div className='flex justify-between items-center w-full h-full px-2 2x1:px-16'>
         {/* imagen */}
         <div></div>
@@ -22,16 +34,16 @@ const Navbar = () => {
             <Link href='/'>
               <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
             </Link>
-            <Link href='/'>
+            <Link href='/#about'>
               <li className='ml-10 text-sm uppercase hover:border-b'>About</li>
             </Link>
-            <Link href='/'>
+            <Link href='/#skills'>
               <li className='ml-10 text-sm uppercase hover:border-b'>Skills</li>
             </Link>
-            <Link href='/'>
+            <Link href='/#projects'>
               <li className='ml-10 text-sm uppercase hover:border-b'>Projects</li>
             </Link>
-            <Link href='/'>
+            <Link href='/#contact'>
               <li className='ml-10 text-sm uppercase hover:border-b'>Contact</li>
             </Link>
           </ul>
